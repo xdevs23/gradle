2e5a4bdc95e13f82960eee43be6311d42d6c68e8
Bo Zhang <bo@gradle.com>
2021-10-12 04:42:08 +0200

    Switch to ge-td-dogfooding.grdev.net
    - Use ge-td-dogfooding as TD backend
    - Add `%ge-td-dogfooding.grdev.net.access.key%`
    
    (cherry picked from commit 8663363e3a6f90dde39845ea9b3f9d835fcac353)

--- build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts
+++ build-logic/jvm/src/main/kotlin/gradlebuild.unittest-and-compile.gradle.kts
@@ -236,8 +236,8 @@ fun configureTests() {
             distribution {
                 enabled.set(true)
                 maxRemoteExecutors.set(0)
-                // Dogfooding TD against ge-experiment until GE 2021.1 is available on e.grdev.net and ge.gradle.org (and the new TD Gradle plugin version 2.0 is accepted)
-                (this as TestDistributionExtensionInternal).server.set(uri("https://ge-experiment.grdev.net"))
+                // Dogfooding TD against ge-td-dogfooding in order to test new features and benefit from bug fixes before they are released
+                (this as TestDistributionExtensionInternal).server.set(uri("https://ge-td-dogfooding.grdev.net"))
             }
         }
 
@@ -249,7 +249,7 @@ fun configureTests() {
                 // No limit; use all available executors
                 distribution.maxRemoteExecutors.set(null)
                 // Dogfooding TD against ge-experiment until GE 2021.1 is available on e.grdev.net and ge.gradle.org (and the new TD Gradle plugin version 2.0 is accepted)
-                (this as TestDistributionExtensionInternal).server.set(uri("https://ge-experiment.grdev.net"))
+                (this as TestDistributionExtensionInternal).server.set(uri("https://ge-td-dogfooding.grdev.net"))
 
                 if (BuildEnvironment.isCiServer) {
                     when {
