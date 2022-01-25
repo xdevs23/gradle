50513c1a6bf660182159763b26c4bedfd2e6b0ec
Kyle Moore <kmoore@gradle.com>
2022-01-24 17:33:00 -0800

    Remove deprecated utility method

--- subprojects/core/src/integTest/groovy/org/gradle/api/tasks/CachedLineEndingSensitivityIntegrationSpec.groovy
+++ subprojects/core/src/integTest/groovy/org/gradle/api/tasks/CachedLineEndingSensitivityIntegrationSpec.groovy
@@ -16,11 +16,8 @@
 
 package org.gradle.api.tasks
 
-import org.gradle.util.TextUtil
-
-
 class CachedLineEndingSensitivityIntegrationSpec extends AbstractLineEndingSensitivityIntegrationSpec {
-    def buildCachePath = TextUtil.normaliseFileSeparators(testDirectory.file("build-cache").absolutePath)
+    def buildCachePath = testDirectory.file("build-cache").toPath().normalize()
 
     def setup() {
         buildFile << """
