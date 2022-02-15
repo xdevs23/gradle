d9fba029f7da0806f5d282059a34748ab66a767d
Bo Zhang <bo@gradle.com>
2022-02-15 16:23:40 +0800

    Use --benchmark

--- run-all-scenarios.sh
+++ run-all-scenarios.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-gradle-profiler --profile buildscan --scenario-file performance.scenarios --project-dir . CleanCompileAllBuild
-gradle-profiler --profile buildscan --scenario-file performance.scenarios --project-dir . TestOnlyChange
-gradle-profiler --profile buildscan --scenario-file performance.scenarios --project-dir . IncrementalBuildWithNonABIChange
-gradle-profiler --profile buildscan --scenario-file performance.scenarios --project-dir . IncrementalBuildWithABIChange
-gradle-profiler --profile buildscan --scenario-file performance.scenarios --project-dir . IncrementalBuildWithBuildLogicChange
+gradle-profiler --benchmark --scenario-file performance.scenarios --project-dir . CleanCompileAllBuild
+gradle-profiler --benchmark --scenario-file performance.scenarios --project-dir . TestOnlyChange
+gradle-profiler --benchmark --scenario-file performance.scenarios --project-dir . IncrementalBuildWithNonABIChange
+gradle-profiler --benchmark --scenario-file performance.scenarios --project-dir . IncrementalBuildWithABIChange
+gradle-profiler --benchmark --scenario-file performance.scenarios --project-dir . IncrementalBuildWithBuildLogicChange
