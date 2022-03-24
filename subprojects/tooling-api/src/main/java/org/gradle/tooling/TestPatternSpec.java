7a6178dd2c40f54db55bf0d79f26a986336f12cc
Donat Csikos <donat@gradle.com>
2022-03-24 17:45:42 +0100

    Add documentation

--- subprojects/tooling-api/src/main/java/org/gradle/tooling/TestPatternSpec.java
+++ subprojects/tooling-api/src/main/java/org/gradle/tooling/TestPatternSpec.java
@@ -21,14 +21,30 @@ import org.gradle.api.Incubating;
 import java.util.Collection;
 
 /**
- * Provides fine-grained test selection within a test task.
+ * Provides infrastructure to select which test classes, methods, and packages will be included in the test execution.
+ * <p>
+ * A complex example:
+ * <pre>
+ * TestLauncher testLauncher = projectConnection.newTestLauncher();
+ * testLauncher.withTestsFor(spec -&gt; {
+ *     spec.forTaskPath(":test")                                    // configure the test selection on the ':test' task
+ *         .includePackage("org.pkg")                               // execute all tests declared the org.pkg package and its sub-packages
+ *         .includeClass("com.MyTest")                              // execute the MyTest test class
+ *         .includeMethod("com.OtherTest", Arrays.asList("verify")) // execute the OtherTest.verify() test method
+ *         .includePattern("io.*")                                  // execute all tests matching to io.*
+ * }).run();
+ * </pre>
+ * <p>
+ * Test classes and methods accept patterns. Patterns follow the rules of <a href="https://docs.gradle.org/current/userguide/java_testing.html#test_filtering">Test Filtering</a>.
+ * <p>
+ * The test execution will fail if any of the selected classes, methods, or patters have no matching tests.
  *
  * @since 7.5
  */
 @Incubating
 public interface TestPatternSpec {
-    TestPatternSpec includePackage(String pkg); // TODO document that it runs all tests from the target package and from the sub-packages
-    TestPatternSpec includePackages(Collection<String> packages); // TODO allow running tests from the target package only
+    TestPatternSpec includePackage(String pkg);
+    TestPatternSpec includePackages(Collection<String> packages);
     TestPatternSpec includeClass(String cls);
     TestPatternSpec includeClasses(Collection<String>  classes);
     TestPatternSpec includeMethod(String clazz, String method);
